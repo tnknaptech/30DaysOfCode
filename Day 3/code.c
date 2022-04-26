0d18f109662524308a199a49029ab15cf64b9f3e
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-04-26 09:34:15 +0530

    Create code.c

---
+++ Day 3/code.c
@@ -0,0 +1,14 @@
+//To Print the table of a number using for loop.
+#include <stdio.h>
+int main()
+{
+    int a, i = 1;
+    printf("Enter Number:");
+    scanf("%d", &a);
+    while (i <= 10)
+    {
+        printf("\n%d * %d = %d", a, i, a * i);
+        i++;
+    }
+    return 0;
+}
