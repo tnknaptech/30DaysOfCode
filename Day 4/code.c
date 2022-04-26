857219dfc9802472369c24886fe455e16057626d
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2022-04-27 00:38:20 +0530

    Create code.c

---
+++ Day 4/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, big;
+    printf("Enter any three numbers");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    if ((a > b) && (a > c))
+    {
+        big = a;
+    }
+    if ((b > a) && (b > c))
+    {
+        big = b;
+    }
+    if ((c > b) && (c > b))
+    {
+        big = c;
+    }
+    printf("Largest Numbers is:%d", big);
+    return 0;
+}
